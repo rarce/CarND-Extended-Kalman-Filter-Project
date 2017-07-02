e9d3f655c5381ae525cb7c7295bf9fd73fa8800d
Roberto Arce <robertoarcee@gmail.com>
2017-07-02 17:56:36 -0400

    Implement predict and update steps in kalman filter

--- src/kalman_filter.h
+++ src/kalman_filter.h
@@ -2,9 +2,9 @@
 #define KALMAN_FILTER_H_
 #include "Eigen/Dense"
 
-class KalmanFilter {
+class KalmanFilter
+{
 public:
-
   // state vector
   Eigen::VectorXd x_;
 
@@ -43,7 +43,7 @@ public:
    * @param Q_in Process covariance matrix
    */
   void Init(Eigen::VectorXd &x_in, Eigen::MatrixXd &P_in, Eigen::MatrixXd &F_in,
-      Eigen::MatrixXd &H_in, Eigen::MatrixXd &R_in, Eigen::MatrixXd &Q_in);
+            Eigen::MatrixXd &H_in, Eigen::MatrixXd &R_in, Eigen::MatrixXd &Q_in);
 
   /**
    * Prediction Predicts the state and the state covariance
@@ -64,6 +64,11 @@ public:
    */
   void UpdateEKF(const Eigen::VectorXd &z);
 
+  /**
+  * Common calculations from UpdateEKF and Update
+  * @param y = z - Hx
+  */
+  void UpdateCommon(const Eigen::VectorXd &y);
 };
 
 #endif /* KALMAN_FILTER_H_ */
